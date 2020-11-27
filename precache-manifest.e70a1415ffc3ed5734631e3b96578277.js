b57cdc29d20f46a1d76f7abfaacf0eca8e5f8129
Chris Tunbridge <blasterjr@gmail.com>
2020-11-27 12:35:12 -0700

    gh-pages

---
+++ precache-manifest.e70a1415ffc3ed5734631e3b96578277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312ef03e2fc8ecbafc63",
+    "url": "/simon-says/css/app.6e8407f4.css"
+  },
+  {
+    "revision": "199cce5b93479778547fc68d42aeaf22",
+    "url": "/simon-says/index.html"
+  },
+  {
+    "revision": "312ef03e2fc8ecbafc63",
+    "url": "/simon-says/js/app.813d0f78.js"
+  },
+  {
+    "revision": "f401fb162736b4bea7a4",
+    "url": "/simon-says/js/chunk-vendors.1b2e9ca1.js"
+  },
+  {
+    "revision": "72b3b243f3c9253634488339b979beb6",
+    "url": "/simon-says/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simon-says/robots.txt"
+  }
+]);
